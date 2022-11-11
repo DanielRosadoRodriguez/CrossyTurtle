7a03bdd6f0c9e3df6975dcd0c5064a23cf7cad5f
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-11 16:07:10 -0600

    fix: changed turtle position when printing game over, the turtle now clears the previous value before printing score

--- scoreboard.py
+++ scoreboard.py
@@ -6,18 +6,18 @@ class Scoreboard(Turtle):
         self.level = 1
         super(Scoreboard, self).__init__()
         self.hideturtle()
-        self.print_score_board()
         self.penup()
         self.goto(x=-250, y=250)
         self.print_score_board()
 
     def update_scoreboard(self):
         self.level += 1
-        self.clear()
         self.print_score_board()
 
     def print_score_board(self):
+        self.clear()
         self.write(arg=f"Score: {self.level}", font=FONT)
 
     def print_game_over(self):
+        self.goto(0, 0)
         self.write(arg="Game Over", font=FONT)
