64fba5f0e50afae1a554c7ce84513b1e55a1115d
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-11 16:22:09 -0600

    feat: add random car generation

--- car_manager.py
+++ car_manager.py
@@ -1,6 +1,7 @@
 import random
 from turtle import Turtle
 from random import choice
+
 COLORS = ["red", "yellow", "blue", "green", "purple", "black"]
 MOV_DISTANCE = 10
 
@@ -9,19 +10,26 @@ class CarManager:
 
     def __init__(self):
         self.all_cars = []
+        self.max_chance = 6
 
     def create_car(self):
-        color = choice(COLORS)
-        random_ycor = random.randint(-220, 220)
-        car = Turtle(shape="square")
-        car.penup()
-        car.color(color)
-        car.setheading(180)
-        car.goto(x=320, y=random_ycor)
-        car.shapesize(stretch_wid=1, stretch_len=2)
-        self.all_cars.append(car)
+
+        car_chance = random.randint(1, self.max_chance)
+        if car_chance == 1:
+            color = choice(COLORS)
+            random_ycor = random.randint(-220, 220)
+            car = Turtle(shape="square")
+            car.penup()
+            car.color(color)
+            car.setheading(180)
+            car.goto(x=320, y=random_ycor)
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            self.all_cars.append(car)
 
     def move_car(self):
         for car in self.all_cars:
             car.forward(MOV_DISTANCE)
 
+    def reduce_max_chance(self):
+        if self.max_chance > 1:
+            self.max_chance -= 1
